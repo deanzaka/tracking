13078d9e428e1872e5ff81d2af0803379453f672
deanzaka <deanzaka.h@gmail.com>
2015-05-09 19:57:40 +0700

    creating new camshift code from scratch

---
+++ camshift/newcamshift.cpp
@@ -0,0 +1,14 @@
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
+#include <ctype.h>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, const char** argv )
+{
+	cout << "test";
+}
