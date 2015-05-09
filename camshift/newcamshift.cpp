8ab37a120cadc61e5d6d8e5a19c265b30a8e76ec
deanzaka <deanzaka.h@gmail.com>
2015-05-09 23:40:44 +0700

    adding region of interest selection

--- camshift/newcamshift.cpp
+++ camshift/newcamshift.cpp
@@ -8,50 +8,94 @@
 using namespace cv;
 using namespace std;
 
+Mat testImage;
+bool selectObject = false;
+int trackObject = 0;
+Point origin;
+Rect selection;
+
+static void onMouse( int event, int x, int y, int, void* )
+{
+    	if( selectObject )
+    	{
+        	selection.x = MIN(x, origin.x);
+        	selection.y = MIN(y, origin.y);
+        	selection.width = std::abs(x - origin.x);
+        	selection.height = std::abs(y - origin.y);
+
+        	selection &= Rect(0, 0, testImage.cols, testImage.rows);
+    	}
+
+    	switch( event )
+    	{
+    		case CV_EVENT_LBUTTONDOWN:
+        		origin = Point(x,y);
+        		selection = Rect(x,y,0,0);
+        		selectObject = true;
+        		break;
+    		case CV_EVENT_LBUTTONUP:
+        		selectObject = false;
+        		if( selection.width > 0 && selection.height > 0 )
+            			trackObject = -1;
+        		break;
+    	}
+}
+
 int main( int argc, const char** argv )
 {
-	Mat testImage, hue, hsv, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
+	Mat hue, hsv, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
 	int hsize = 16;
 	float hranges[] = {0,180};
 	const float* phranges = hranges;
 
 	namedWindow("test image", 0);
 	namedWindow("histogram image", 0);
-	testImage  = imread("digimon.jpg");
-	imshow("test image", testImage);
+	setMouseCallback( "test image", onMouse, 0 );
 
-	cvtColor(testImage, hsv, CV_BGR2HSV);
-	int ch[]={0,0};
-	hue.create(hsv.size(), hsv.depth());
-	mixChannels(&hsv, 1, &hue, 1, ch, 1);								// copying only hue values
-	
-	calcHist(&hue, 1, 0, Mat(), hist, 1, &hsize, &phranges);				// calculate histogram
-	normalize(hist, hist, 0,255, CV_MINMAX);							// normalize value to min 0 to max 255
-	calcBackProject( &hue, 1, 0, hist, backproj, &phranges, 1, true );		// calculate back projection of image, by histogram
+	while(1) {
+		testImage  = imread("digimon.jpg");
+		cvtColor(testImage, hsv, CV_BGR2HSV);
+		int ch[]={0,0};
+		hue.create(hsv.size(), hsv.depth());
+		mixChannels(&hsv, 1, &hue, 1, ch, 1);								// copying only hue values
+		
+		calcHist(&hue, 1, 0, Mat(), hist, 1, &hsize, &phranges);				// calculate histogram
+		normalize(hist, hist, 0,255, CV_MINMAX);							// normalize value to min 0 to max 255
+		calcBackProject( &hue, 1, 0, hist, backproj, &phranges, 1, true );		// calculate back projection of image, by histogram
 
-	histimg = Scalar::all(0);
-      	int binW = histimg.cols / hsize;
-      	Mat buf(1, hsize, CV_8UC3);
-      	for( int i = 0; i < hsize; i++ )
-          buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
-      	cvtColor(buf, buf, CV_HSV2BGR);
+		histimg = Scalar::all(0);
+	      	int binW = histimg.cols / hsize;
+	      	Mat buf(1, hsize, CV_8UC3);
+	      	for( int i = 0; i < hsize; i++ )
+	          buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
+	      	cvtColor(buf, buf, CV_HSV2BGR);
 
-      	for( int i = 0; i < hsize; i++ )
-      {
-          int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
-          rectangle( histimg, Point(i*binW,histimg.rows),
-                     Point((i+1)*binW,histimg.rows - val),
-                     Scalar(buf.at<Vec3b>(i)), -1, 8 );
-      }
+	      	for( int i = 0; i < hsize; i++ )
+	      {
+	          int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
+	          rectangle( histimg, Point(i*binW,histimg.rows),
+	                     Point((i+1)*binW,histimg.rows - val),
+	                     Scalar(buf.at<Vec3b>(i)), -1, 8 );
+	      }
 
-      	if(!histimg.empty()){
-       	imshow("histogram image", histimg);
-    	}
-      
+	      	imshow("histogram image", histimg);
+	    	
+	  	if( selectObject && selection.width > 0 && selection.height > 0 )
+	       {
+	            	Mat roi(testImage, selection);
+	            	bitwise_not(roi, roi);
+	       }
 
-	if (waitKey(0) == 27) //wait for 'esc' key press for 1ms. If 'esc' key is pressed, break loop
-      {
-	       cout << "\nESC: Closing Application\n";
-	       return 0;
-      }
+	       cout << "\n selection width: " << selection.width << "\t";    
+	       cout << "selection height: " << selection.height << "\t";
+	       cout << " select object: " << selectObject << "\n";
+
+		imshow("test image", testImage);
+
+		if (waitKey(1) == 27) //wait for 'esc' key press for 1ms. If 'esc' key is pressed, break loop
+	      {
+		       cout << "\nESC: Closing Application\n";
+		       return 0;
+	      }
+	}
 }
